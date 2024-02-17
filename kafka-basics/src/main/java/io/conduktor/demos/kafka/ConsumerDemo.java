9aed20fa32ed5b4126b83096e3d4edbbe7b43662
Lei Wang <mail.lei.wang@gmail.com>
2024-02-17 08:49:33 +0100

    consumer in progress

--- kafka-basics/src/main/java/io/conduktor/demos/kafka/ConsumerDemo.java
+++ kafka-basics/src/main/java/io/conduktor/demos/kafka/ConsumerDemo.java
@@ -1,8 +1,11 @@
 package io.conduktor.demos.kafka;
 
 import lombok.extern.slf4j.Slf4j;
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.apache.kafka.clients.consumer.ConsumerRecords;
 import org.apache.kafka.clients.consumer.KafkaConsumer;
 
+import java.time.Duration;
 import java.util.Collections;
 import java.util.Properties;
 
@@ -15,7 +18,7 @@ public class ConsumerDemo {
 
         // create Producer Properties
         Properties properties = new KafkaPropertiesBuilder()
-                .bootStrapServersLocal()
+                .bootStrapServersUpstash()
                 .keyValueStringDeserializer()
                 .groupId(groupId)
                 .autoOffsetReset(KafkaPropertiesBuilder.AutoOffsetReset.EARLIEST)
@@ -24,6 +27,14 @@ public class ConsumerDemo {
         // create a consumer
         try (KafkaConsumer<String, String> kafkaConsumer = new KafkaConsumer<>(properties)) {
             kafkaConsumer.subscribe(Collections.singletonList(topic));
+            while (true) {
+                log.info("Polling.");
+                ConsumerRecords<String, String> consumerRecords = kafkaConsumer.poll(Duration.ofSeconds(1));
+                for (ConsumerRecord<String, String> consumerRecord : consumerRecords) {
+                    log.info("Key: " + consumerRecord.key() + ", Value: " + consumerRecord.value());
+                    log.info("Partition: " + consumerRecord.partition() + ", Offset: " + consumerRecord.offset());
+                }
+            }
         }
     }
 }
