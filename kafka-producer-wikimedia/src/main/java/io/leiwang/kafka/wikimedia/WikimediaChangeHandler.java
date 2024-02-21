05330204039c9a120ff40444708c9c3fed4f7377
Lei Wang <mail.lei.wang@gmail.com>
2024-02-21 13:25:35 +0100

    producer ready

--- kafka-producer-wikimedia/src/main/java/io/leiwang/kafka/wikimedia/WikimediaChangeHandler.java
+++ kafka-producer-wikimedia/src/main/java/io/leiwang/kafka/wikimedia/WikimediaChangeHandler.java
@@ -5,6 +5,7 @@ import com.launchdarkly.eventsource.background.BackgroundEventHandler;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.ProducerRecord;
 
 @RequiredArgsConstructor
 @Slf4j
@@ -24,16 +25,18 @@ public class WikimediaChangeHandler implements BackgroundEventHandler {
 
     @Override
     public void onMessage(String event, MessageEvent messageEvent) throws Exception {
-
+        log.info(messageEvent.getData());
+        // asynchronous
+        kafkaProducer.send(new ProducerRecord<>(topic, messageEvent.getData()));
     }
 
     @Override
     public void onComment(String comment) throws Exception {
-
+        // nothing here
     }
 
     @Override
     public void onError(Throwable t) {
-
+        log.error("Error in stream reading", t);
     }
 }
--- kafka-producer-wikimedia/src/main/java/io/leiwang/kafka/wikimedia/WikimediaChangesProducer.java
+++ kafka-producer-wikimedia/src/main/java/io/leiwang/kafka/wikimedia/WikimediaChangesProducer.java
@@ -1,7 +1,6 @@
 package io.leiwang.kafka.wikimedia;
 
 import com.launchdarkly.eventsource.EventSource;
-import com.launchdarkly.eventsource.StreamException;
 import com.launchdarkly.eventsource.background.BackgroundEventHandler;
 import com.launchdarkly.eventsource.background.BackgroundEventSource;
 import io.leiwang.kafka.common.KafkaPropertiesBuilder;
@@ -9,24 +8,26 @@ import org.apache.kafka.clients.producer.KafkaProducer;
 
 import java.net.URI;
 import java.util.Properties;
+import java.util.concurrent.TimeUnit;
 
 public class WikimediaChangesProducer {
-    public static void main(String[] args) throws StreamException {
+    public static void main(String[] args) throws InterruptedException {
         // create Producer properties
         Properties properties = new KafkaPropertiesBuilder()
-                .bootStrapServersLocal()
+                .bootStrapServersUpstash()
+                //.bootStrapServersLocal()
                 .keyValueStringSerializer()
                 .build();
         // create the Producer
         KafkaProducer<String, String> producer = new KafkaProducer<>(properties);
         String topic = "wikimedia.recentchange";
-        BackgroundEventHandler backgroundEventHandler = null;
+        BackgroundEventHandler backgroundEventHandler = new WikimediaChangeHandler(producer, topic);
         String url = "https://stream.wikimedia.org/v2/stream/recentchange";
         EventSource.Builder eventSourceBuilder = new EventSource.Builder(URI.create(url));
-        BackgroundEventSource.Builder backGroundEventSourceBuilder = new BackgroundEventSource.Builder(backgroundEventHandler,eventSourceBuilder);
-        EventSource eventSource = eventSourceBuilder.build();
-        BackgroundEventSource backgroundEventSource = backGroundEventSourceBuilder.build();
-        eventSource.start();
-        backgroundEventSource.start();
+        BackgroundEventSource.Builder backGroundEventSourceBuilder = new BackgroundEventSource.Builder(backgroundEventHandler, eventSourceBuilder);
+        try (BackgroundEventSource backgroundEventSource = backGroundEventSourceBuilder.build()) {
+            backgroundEventSource.start();
+        }
+        TimeUnit.SECONDS.sleep(5);
     }
 }
