c8d76fefb38a9d8710d5d62c2a9c6e07243503fa
Lei <mail.lei.wang@gmail.com>
2025-01-04 21:50:59 +0100

    topics done

--- infrastructure/guide/example.sh
+++ infrastructure/guide/example.sh
@@ -1,17 +1,19 @@
 #!/bin/bash
 
 # start natively installed kafka:
+export KAFKA_LOG4J_OPTS="-Dlog4j.configuration=file:/etc/kafka/log4j.properties"
 kafka-storage.sh random-uuid
 kafka-storage.sh format -t X2edeaTuRSCYe1J8t93G0Q -c /etc/kafka/kraft/server.properties
-export KAFKA_LOG4J_OPTS="-Dlog4j.configuration=file:/etc/kafka/log4j.properties"
 kafka-server-start.sh /etc/kafka/kraft/server.properties 
 
 # stop natively installed kafka:
-kafka-topics.sh --create --topic test-topic --bootstrap-server localhost:9092
+kafka-server-stop.sh
 
 # create a tppic,
 # when --partitions not specified, then num.partitions property is used (typical value is 1)
-kafka-topics.sh --create --topic test-topic --partitions  --bootstrap-server localhost:9092
+# --replication-factor: how many copies of each partition are stored across different brokers
+# --bootstrap-server: it is recommended to provid at least 2, so that when one is offline, the other could process the request
+kafka-topics.sh --create --topic test-topic --partitions 3 --replication-factor 3 --bootstrap-server localhost:9092,localhost:9094
 # list topics
 kafka-topics.sh --list --bootstrap-server localhost:9092
 # run list topics command from host of a docker compose file
