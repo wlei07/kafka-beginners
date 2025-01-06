a4914eecf306169bab39e8e9ff542b9d2ce39811
Lei <mail.lei.wang@gmail.com>
2025-01-06 21:49:18 +0100

    consumers in progress

--- infrastructure/guide/example.sh
+++ infrastructure/guide/example.sh
@@ -14,9 +14,21 @@ kafka-server-stop.sh
 # --replication-factor: how many copies of each partition are stored across different brokers
 # --bootstrap-server: it is recommended to provid at least 2, so that when one is offline, the other could process the request
 kafka-topics.sh --create --topic test-topic --partitions 3 --replication-factor 3 --bootstrap-server localhost:9092,localhost:9094
-# list topics
+# list topics: display only names of the topics
 kafka-topics.sh --list --bootstrap-server localhost:9092
 # run list topics command from host of a docker compose file
 docker compose exec kafka-1 /opt/bitnami/kafka/bin/kafka-topics.sh --list --bootstrap-server localhost:9092
-# describe a topic, its leader broker, etc.
+# describe a topic: display more information than --list, e.g., its leader broker, etc.
 kafka-topics.sh --describe --bootstrap-server localhost:9092 test-topic2
+# delete a topic
+kafka-topics.sh --delete --topic topic1 --bootstrap-server localhost:9092
+# update a topic:
+kafka-configs.sh --bootstrap-server localhost:9092 --alter --entity-type topics --entity-name topic2 --add-config retention.ms=43200000
+# parititions could ONLY be increased, NOT decreased
+kafka-topics.sh --bootstrap-server localhost:9092 --alter --topic topic2 --partitions 6
+
+# send a message to a topic
+kafka-console-producer.sh --bootstrap-server localhost:9092,localhost:9096 --topic topic2 
+# send a message to a topic, note that when a topic does not exist, it will create a new one by default (can be configured to disable it).
+kafka-console-producer.sh --bootstrap-server localhost:9092,localhost:9096 --topic topic2 --property "parse.key=true" --property "key.separator=:"
+
