73d364f88cb82cf482ffa3934f4812fe286ab00f
Lei <mail.lei.wang@gmail.com>
2025-01-19 21:55:23 +0100

    extended with producer retry

--- products-microservice/src/main/java/com/example/products_microservice/KafkaConfig.java
+++ products-microservice/src/main/java/com/example/products_microservice/KafkaConfig.java
@@ -18,14 +18,6 @@ public class KafkaConfig {
                 .partitions(3)
                 .replicas(3)
                 // min.insync.replicas for number of producers that must response for acknowledgement
-
-                // spring.kafka.producer.reties=10, default is 2147483647
-                // spring.kafka.producer.properties.retry.backoff.ms=1000, default is 100ms, time interval between reties.
-                // AND alternatively:
-                // spring.kafka.producer.properties.delivery.timeout.ms=120000, the maximum time producer can spend re-trying to deliver the message.
-                // NOTE: delivery.timeout.ms >= linger.ms + request.timeout.ms
-                // spring.kafka.producer.properties.linger.ms: wait and batch, then send all batched messages in one time.
-                // spring.kafka.producer.properties.request.timeout.ms=30000, wait for a response after sending a request
                 .config("min.insync.replicas", "2")
                 .build();
     }
