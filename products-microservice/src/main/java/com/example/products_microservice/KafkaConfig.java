193eb924fa850f35779c703de816010bff2adee3
Lei <mail.lei.wang@gmail.com>
2025-01-27 21:45:29 +0100

    done producers course section

--- products-microservice/src/main/java/com/example/products_microservice/KafkaConfig.java
+++ products-microservice/src/main/java/com/example/products_microservice/KafkaConfig.java
@@ -17,30 +17,24 @@ import java.util.Map;
 public class KafkaConfig {
     @Value("${app.topic-name}")
     private String topicName;
-
     @Value("${spring.kafka.producer.bootstrap-servers}")
     private String bootstrapServers;
-
     @Value("${spring.kafka.producer.key-serializer}")
     private String keySerializer;
-
     @Value("${spring.kafka.producer.value-serializer}")
     private String valueSerializer;
-
     @Value("${spring.kafka.producer.acks}")
     private String acks;
-
     @Value("${spring.kafka.producer.properties.delivery.timeout.ms}")
     private String deliveryTimeout;
-
     @Value("${spring.kafka.producer.properties.linger.ms}")
     private String linger;
-
     @Value("${spring.kafka.producer.properties.request.timeout.ms}")
     private String requestTimeout;
-
     @Value("${spring.kafka.producer.properties.enable.idempotence}")
     private boolean idempotence;
+    @Value("${spring.kafka.producer.properties.max.in.flight.requests.per.connection}")
+    private Integer inflightRequests;
 
     @Bean
     NewTopic createTopic() {
@@ -64,6 +58,8 @@ public class KafkaConfig {
         config.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, keySerializer);
         config.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, valueSerializer);
         config.put(ProducerConfig.ACKS_CONFIG, acks);
+        // config.put(ProducerConfig.RETRIES_CONFIG, Integer.MAX_VALUE);
+        // NOTE the above RETRIES_CONFIG is commented out because of the below 3 options are alternative
         config.put(ProducerConfig.DELIVERY_TIMEOUT_MS_CONFIG, deliveryTimeout);
         config.put(ProducerConfig.LINGER_MS_CONFIG, linger);
         config.put(ProducerConfig.REQUEST_TIMEOUT_MS_CONFIG, requestTimeout);
@@ -72,6 +68,7 @@ public class KafkaConfig {
         // retries > 0, AND
         // max.in.flight.requests.per.connection <= 5
         config.put(ProducerConfig.ENABLE_IDEMPOTENCE_CONFIG, idempotence);
+        config.put(ProducerConfig.MAX_IN_FLIGHT_REQUESTS_PER_CONNECTION, inflightRequests);
         return config;
     }
 
